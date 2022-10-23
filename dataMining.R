01c458cb4414f4614f5def03a97daab1f6d9d85f
samerelearning <samerelearning@outlook.com>
2022-10-24 01:05:56 +0300

    Renaming variables and deleting columns

--- dataMining.R
+++ dataMining.R
@@ -1,1 +1,15 @@
 dataset <- read.csv("Dataset/DANE_PRZYGOTOWANE_80.csv")
+
+View(dataset)
+
+install.packages("tidyverse")
+library("tidyverse")
+
+#Renaming variables and eliminating columns
+my_dataset <- dataset %>%
+  select(Year, Player, height, weight, Pos, G, MP, X3PAr, FTr, TRB,
+         X3P, X3PA, X2P, X2PA, FT, FTA, TRB, AST, STL, BLK, TOV, PTS) %>%
+  rename(Games = G) %>%
+  mutate(height = height / 100)
+
+View(my_dataset)
