fc7c5de42d3eb149469d7fa07aa84873406196b4
samerelearning <samerelearning@outlook.com>
2022-10-24 23:43:24 +0300

    implementing linear regression

--- dataMining.R
+++ dataMining.R
@@ -9,16 +9,25 @@ str(dataset)
 glimpse(dataset)
 head(dataset)
 tail(dataset)
-View(dataset)
 names(dataset)
 length(dataset)
+View(dataset)
 
 #Renaming variables and eliminating columns
 my_dataset <- dataset %>%
-  select(Year, Player, height, weight, Pos, G, MP,
-         X3P, X3PA, X2P, X2PA, FT, AST, STL, BLK, TOV, PTS) %>%
-  filter(Pos == "SG" | Pos == "PG") %>%
-  rename(Games = G) %>%
+  select(Year, Player, height, weight, Pos, MP, X3P, X3PA, PTS) %>%
+  filter(Pos == "SG" | Pos == "PG", MP > 2000) %>%
   mutate(height = height / 100, X3P = X3P * 100)
 
 View(my_dataset)
+
+summary(my_dataset)
+boxplot(my_dataset$X3P)
+hist(my_dataset$X3P)
+
+#Linear regression
+linear_regression <- my_dataset %>%
+  lm(my_dataset$X3P ~ my_dataset$Year, data = .) %>%
+  summary()
+
+hist(linear_regression$residuals)
