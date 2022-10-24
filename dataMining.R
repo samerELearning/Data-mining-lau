69a427a51df61476df7d8fff30dcce5d84d28633
samerelearning <samerelearning@outlook.com>
2022-10-24 21:45:42 +0300

    filtering unwanted obeservations

--- dataMining.R
+++ dataMining.R
@@ -1,15 +1,24 @@
 dataset <- read.csv("Dataset/DANE_PRZYGOTOWANE_80.csv")
 
-View(dataset)
-
-install.packages("tidyverse")
+#install.packages("tidyverse")
 library("tidyverse")
+
+#Exploring the data set
+dim(dataset)
+str(dataset)
+glimpse(dataset)
+head(dataset)
+tail(dataset)
+View(dataset)
+names(dataset)
+length(dataset)
 
 #Renaming variables and eliminating columns
 my_dataset <- dataset %>%
-  select(Year, Player, height, weight, Pos, G, MP, X3PAr, FTr, TRB,
-         X3P, X3PA, X2P, X2PA, FT, FTA, TRB, AST, STL, BLK, TOV, PTS) %>%
+  select(Year, Player, height, weight, Pos, G, MP,
+         X3P, X3PA, X2P, X2PA, FT, AST, STL, BLK, TOV, PTS) %>%
+  filter(Pos == "SG" | Pos == "PG") %>%
   rename(Games = G) %>%
-  mutate(height = height / 100)
+  mutate(height = height / 100, X3P = X3P * 100)
 
 View(my_dataset)
